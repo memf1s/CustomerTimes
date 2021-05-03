b673f4aec3a29cb48518e2771862dca6a300bcfa
Stas Stepanov <memf1sio@gmail.com>
2021-05-03 19:51:45 +0300

    imported project commit

---
+++ VacancyBatch/.sfdx/typings/lwc/sobjects/Vacancy__ChangeEvent.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/schema/Vacancy__ChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Vacancy__ChangeEvent.Account__c" {
+  const Account__c:any;
+  export default Account__c;
+}
